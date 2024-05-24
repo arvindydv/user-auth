0bb7081145d9a7118daf241cffc2232c0b6a170c
arvindydv <arvindydv03@gmail.com>
2024-05-24 12:58:18 +0530

    remove comments

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import xss from "xss-clean";
-import rateLimit from "express-rate-limit";
+
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 
-app.use(helmet()); // Adds security headers
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(xss()); //  inputs sanitization.
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-});
 
 app.use(limiter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
